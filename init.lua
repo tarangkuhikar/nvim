052aef834690f22a6412eda94c6664ad814d2b84
tarangkuhikar <tarangkuhikar@gmail.com>
2023-11-01 14:25:20 +0530

    added terminal remapping.

--- init.lua
+++ init.lua
@@ -292,6 +292,7 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('i', 'jk', '<Esc>')
+vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
