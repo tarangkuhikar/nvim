564985ad2b2390d93f891cda8f73ea6eae5f6fdf
Tarang Kuhikar <tarangkuhikar@gmail.com>
2024-02-23 16:43:09 +0530

    remove redundand field from lua lsp

--- init.lua
+++ init.lua
@@ -613,7 +613,7 @@ local servers = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
       -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      diagnostics = { disable = { 'missing-fields' }, globals = { 'vim' } },
+      diagnostics = { disable = { 'missing-fields' } },
     },
   },
 }
