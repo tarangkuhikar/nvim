9f32129a45c15d9ec073be6549cac2a30a0420b1
Tarang Kuhikar <tarang.devstree@gmail.com>
2024-07-22 14:48:31 +0530

    Make config more like upstream for easy merging.

--- init.lua
+++ init.lua
@@ -638,6 +638,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
       })
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
         handlers = {
@@ -814,19 +815,12 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-      style = 'night',
-      transparent = true,
-      styles = {
-        sidebars = 'transparent',
-      },
-    },
+    priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight'
+      vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
