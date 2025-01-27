794d2c1018a520fc018c3a243288c98e7186c3ac
Tarang Kuhikar <tarang.k@devstree.in>
2025-01-27 10:23:33 +0530

    add search only in csharp files

--- init.lua
+++ init.lua
@@ -425,6 +425,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sc', function()
+        builtin.live_grep { type_filter = 'csharp' }
+      end, { desc = '[S]earch current [W]ord in [C]Sharp files' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
